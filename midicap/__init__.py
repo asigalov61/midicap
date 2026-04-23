782c41f286f7f11a0f4fb933ef059ccbf4514f56
Alex <alexlev61@proton.me>
2026-04-22 23:00:02 -0700

    Add files via upload

--- midicap/__init__.py
+++ midicap/__init__.py
@@ -1,4 +1,4 @@
-from .midicap import generate_midi_captions, generate_midi_description(
+from .midicap import generate_midi_captions, generate_midi_description
 from .analyzer import analyze_midi
 from .fast_analyzer import analyze_midi_fast
 from .representations import numeric_dict_to_tokenized_representation, numeric_dict_to_feature_vector
